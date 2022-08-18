22a47f542aca4db117f62ece1a4b1688c3f31cfd
M.Inzamam Malik <malikasinger1@gmail.com>
2022-08-18 22:47:43 +0500

    class code

--- 03. nodejs/7. express login signup with database/server.mjs
+++ 03. nodejs/7. express login signup with database/server.mjs
@@ -1,6 +1,40 @@
 import express from "express"
 import cors from "cors"
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
+import mongoose from 'mongoose';
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+let dbURI = 'mongodb+srv://abc:abc@cluster0.xwbyne9.mongodb.net/socialMediaBase?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function() {//connected
+    console.log("Mongoose is connected");
+});
+
+mongoose.connection.on('disconnected', function() {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function(err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function() {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function() {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+
+
+
 
 
 const app = express();
